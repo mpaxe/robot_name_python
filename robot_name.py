9d48e413c22f168d968f2aabb03a913c4c458ea2
mpaxe <mpaxe@hotmail.com>
2019-06-24 23:03:52 +0200

    Primera prueba robot

---
+++ robot_name.py
@@ -0,0 +1,19 @@
+import string
+import random
+class Robot(object):
+
+    def __init__(self):
+        self.name = Robot.name()
+   
+    def name():
+        name = ""
+        for i in range(5):
+            if i < 2:
+                name += random.choice(string.ascii_uppercase)
+            else:
+                name += str(random.randrange(10))
+        return name
+   
+    def reset(self):
+        self.name = "AA000"
+        
